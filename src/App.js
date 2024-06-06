aa53a42bbab9874a4039ffadfdd81b762f1e176f
agrwl-prankit <agrwnlprankit77@gmail.com>
2024-06-06 17:41:52 +0530

    Add fragment and split components by feature

--- src/App.js
+++ src/App.js
@@ -1,53 +1,18 @@
 import './App.css';
-import {useState} from 'react';
-import {CORE_CONCEPTS, EXAMPLES} from './data';
+import {Fragment} from 'react';
 import { Header } from './components/Header/Header';
-import { CoreConcepts } from './components/CoreComponent/CoreConcepts';
-import { TabButton } from './components/TabButton/TabButton';
+import { Examples } from './components/Example/Example';
+import { CoreConcept } from './components/CoreConcepts/CoreConcepts';
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
-
-  function handleClick(selectedButton){
-    setSelectedTopic(selectedButton);
-  }
-
-  let tabContent = <p>Please select a topic first</p>;
-  if (selectedTopic){
-    tabContent = <div id='tab-content'>
-      <h3>{EXAMPLES[selectedTopic].title}</h3>
-      <p>{EXAMPLES[selectedTopic].description}</p>
-      <pre>
-        <code>
-          {EXAMPLES[selectedTopic].code}
-        </code>
-      </pre>
-    </div>
-  }
-
   return (
-    <div>
+    <Fragment>
       <Header></Header>
       <main>
-        <section id='core-concepts'>
-          <h2>Core concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((item) => <CoreConcepts {...item}/>)}
-          </ul>
-        </section>
-        <section id='examples'>
-          <h2>Examples</h2>
-          <menu>
-            {/* children prop: content between component tag */}
-            <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleClick('components')}>Components</TabButton>
-            <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleClick('jsx')}>JSX</TabButton>
-            <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleClick('props')}>Props</TabButton>
-            <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleClick('state')}>State</TabButton>
-          </menu>
-          {tabContent}
-        </section>
+        <CoreConcept></CoreConcept>
+        <Examples></Examples>
       </main>
-    </div>
+    </Fragment>
   );
 }
 
---
+++ src/components/Example/Example.js
@@ -0,0 +1,41 @@
+import { TabButton } from "../TabButton/TabButton"
+import { EXAMPLES } from "../../data";
+import { useState } from "react";
+
+export function Examples() {
+
+    const [selectedTopic, setSelectedTopic] = useState();
+
+    function handleClick(selectedButton){
+        setSelectedTopic(selectedButton);
+    }
+
+    let tabContent = <p>Please select a topic first</p>;
+    if (selectedTopic){
+        tabContent = <div id='tab-content'>
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+            <code>
+            {EXAMPLES[selectedTopic].code}
+            </code>
+        </pre>
+        </div>
+    }
+
+    return(
+        <>
+            <section id='examples'>
+                <h2>Examples</h2>
+                <menu>
+                {/* children prop: content between component tag */}
+                <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleClick('components')}>Components</TabButton>
+                <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleClick('jsx')}>JSX</TabButton>
+                <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleClick('props')}>Props</TabButton>
+                <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleClick('state')}>State</TabButton>
+                </menu>
+                {tabContent}
+            </section>
+        </>
+    )
+}
