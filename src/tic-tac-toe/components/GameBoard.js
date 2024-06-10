665888daa15b82448ddb20ab7d5c8c5f9531f6ea
agrwl-prankit <agrwnlprankit77@gmail.com>
2024-06-10 15:59:44 +0530

    optimize code using computed  values and removing state management and derived state from props

--- src/tic-tac-toe/components/GameBoard.js
+++ src/tic-tac-toe/components/GameBoard.js
@@ -6,24 +6,34 @@ let initialGameBoard = [
     [null, null, null],
 ]
 
-export default function GameBoard({onSelectSquare, activePlayerSymbol}){
-    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+export default function GameBoard({onSelectSquare, turns}){
+    let gameBoard = initialGameBoard;
 
-    function handleSelectSquare(rowIndex, colIndex) {
-        setGameBoard((prevGameBoard) => {
-            const updateBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-            updateBoard[rowIndex][colIndex] = activePlayerSymbol;
-            return updateBoard;
-        });
-        onSelectSquare();
+    for (const turn of turns){
+        const {square, player} = turn;
+        const {row, col} = square;
+
+        gameBoard[row][col] = player;
     }
+
+
+    // const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    // function handleSelectSquare(rowIndex, colIndex) {
+    //     setGameBoard((prevGameBoard) => {
+    //         const updateBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+    //         updateBoard[rowIndex][colIndex] = activePlayerSymbol;
+    //         return updateBoard;
+    //     });
+    //     onSelectSquare();
+    // }
 
     return (
         <ol id='game-board'>
             {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
                 <ol>
                     {row.map((playerSymbol, colIndex) => <li key={colIndex}>
-                        <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                        <button onClick={()=> onSelectSquare(rowIndex, colIndex)}>
                             {playerSymbol}</button>
                     </li>)}
                 </ol>
---
+++ src/tic-tac-toe/components/Header.js
@@ -0,0 +1,6 @@
+export default function Header(){
+    return(<>
+    <img src="game-logo.png" alt="Hand drawn tic-tac-toe game board"/>
+    <h1>Tic-Tac-Toe</h1>
+    </>)
+}
